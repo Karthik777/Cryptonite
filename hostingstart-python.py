d11cd481e1757bfc627d7aa1584f81429b0ec022
Karthik777 <karthik.cool7777@gmail.com>
2017-11-10 13:04:24 +0800

    update changes

--- hostingstart-python.py
+++ hostingstart-python.py
@@ -4,7 +4,7 @@ from app import *
 
 def application(environ, start_response):
     start_response(b'200 OK', [(b'Content-Type', b'text/html')])
-    yield runServer()
-    # with open ("hostingstart-python.html", "r") as hostingstart_file:
-    #     hosting = hostingstart_file.read()
-    #     yield hosting.encode('utf8').replace(b'PYTHON_VERSION', platform.python_version().encode('utf8'))
+    # yield runServer()
+    with open ("hostingstart-python.html", "r") as hostingstart_file:
+         hosting = hostingstart_file.read()
+         yield hosting.encode('utf8').replace(b'PYTHON_VERSION', platform.python_version().encode('utf8'))
